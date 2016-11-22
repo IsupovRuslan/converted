a7609223efb642258a28fb1d4e8787ecb73dc8e5
Ruslan-Ruslan <zincusin@gmail.com>
2016-11-22 22:32:42 +0300

    converted

--- JS/sort.js
+++ JS/sort.js
@@ -1,1 +1,61 @@
-console.log('sort.js - Здесь должна была родиться сортировка таблицы..')
+  function sort(colNum, type, dir){                      //Объявление функции сортировки
+    var table = document.getElementById('stats-table-italy');
+    var tbody = table.getElementsByTagName('tbody')[0];
+    var rowsArray = [].slice.call(tbody.rows);
+    var compare;
+    switch (dir){
+      case "desc":                                     //Убывание
+        switch (type){
+          case "number":
+            compare = function(rowA, rowB){
+              return rowB.cells[colNum].innerHTML - rowA.cells[colNum].innerHTML;
+            };
+            break;
+
+          case "string":
+            compare = function(rowA, rowB){
+              return rowA.cells[colNum].children[0].nextSibling.innerHTML < rowB.cells[colNum].children[0].nextSibling.innerHTML ? 1 : -1;
+            };
+          break;
+      }
+      break;
+
+      case "inc":                                     //Возрастание
+        switch (type) {
+           case "number":
+             compare = function(rowA, rowB){
+                return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+              };
+           break;
+
+          case "string":
+            compare = function(rowA, rowB){
+               return rowA.cells[colNum].children[0].nextSibling.innerHTML > rowB.cells[colNum].children[0].nextSibling.innerHTML ? 1 : -1;
+            };
+          break;
+        }
+      break;
+    }
+    rowsArray.sort(compare);
+    table.removeChild(tbody);
+    for (var i = 0; i < rowsArray.length; i++){
+      tbody.appendChild(rowsArray[i]);
+    }
+      table.appendChild(tbody);
+  }
+                                                          //Инициализация
+var App = document.getElementById('App');
+var prevName;
+  App.onclick = function(event){
+  /*  console.log(event);*/                               //для отладки
+    if (event.target.tagName != 'TH'){
+      return;
+    }
+    else if (event.target == prevName){
+      prevName = "";
+      sort(event.target.cellIndex, event.target.getAttribute('data-type'), "desc");
+      return;
+    }
+      prevName = event.target;
+      sort(event.target.cellIndex, event.target.getAttribute('data-type'), "inc");
+  };
--- JS/table.js
+++ JS/table.js
@@ -1,43 +1,44 @@
-$.getJSON('/JSONs/data.json', function(data) {                                                   //Запрос JQuery на файл --> Парсинг
+$.getJSON('/JSONs/data.json', function(data){                                                   //Запрос JQuery на файл --> Парсинг
 
       var index=0;
-      var table="<table>";
-      table +="<thead>";
+      var table="<table id='stats-table-italy' >";
+      table +="<thead>"
       table +="<tr>"+                                                                            //Создание заголовка для столбцов
-                "<th data-type='number' class='place'></th>"+
-                "<th data-type='string' class='team'>Команда</th>"+
-                "<th data-type='number' class='stats-header' title='border-r'>М</th>"+
-                "<th data-type='number' class='stats-header' title='border-c'>В</th>"+
-                "<th data-type='number' class='stats-header' title='border-c'>Н</th>"+
-                "<th data-type='number' class='stats-header' title='border-c'>П</th>"+
-                "<th data-type='number' class='stats-header' title='border-c'>Заб.</th>"+
-                "<th data-type='number' class='stats-header' title='border-c'>Проп.</th>"+
-                "<th data-type='number' class='stats-score-header' title='border-l'>О</th>"+
+                "<th  data-type='number' class='place'></th>"+
+                "<th  data-type='string' class='team'>Команда</th>"+
+                "<th  data-type='number' class='stats-header' title='border-r' id='header-matches'>М</th>"+
+                "<th  data-type='number' class='stats-header' title='border-c' id='header-win'>В</th>"+
+                "<th  data-type='number' class='stats-header' title='border-c' id='header-draw'>Н</th>"+
+                "<th  data-type='number' class='stats-header' title='border-c' id='header-lose'>П</th>"+
+                "<th  data-type='number' class='stats-header' title='border-c' id='header-goals'>Заб.</th>"+
+                "<th  data-type='number' class='stats-header' title='border-c' id='header-conc-goals'>Проп.</th>"+
+                "<th  data-type='number' class='stats-score-header' title='border-l'>О</th>"+
               "</tr>";
+
       table +="</thead>";
       table +="<tbody>";
-      for (var i=0, count=data.teams.length; i<count; i++) {                               // Цикл для заполнения таблицы
+      for (var i=0, count=data.teams.length; i<count; i++){                               // Цикл для заполнения таблицы
         table+="<tr id='z_"+index+"'>";                                                    //Присваивание ID для сортировки..так и не смог реализовать свой метод=(
-        table+="<td data-type='number' title='color"+data.teams[i].color+"'>" + data.teams[i].place +"</td>"; //Кривой, но рабочий способ назначения цвета ячейки,
-        table+="<td data-type='string' class='teams'>" +                                                      //зависящий от места в турнирной таблице
+        table+="<td  title='color"+data.teams[i].color+"'>" + data.teams[i].place +"</td>"; //Кривой, но рабочий способ назначения цвета ячейки,
+        table+="<td  class='teams'>" +                                                      //зависящий от места в турнирной таблице
                "<img src='/img/flag.png'>"+
                "<a href="+data.teams[i].tag_url+">"+data.teams[i].name +"</a>"
                "</td>";
-        table+="<td title='border-r'>" + data.teams[i].matches +"</td>";
-        table+="<td title='border-c'>" + data.teams[i].win +"</td>";
-        table+="<td title='border-c'>" + data.teams[i].draw +"</td>";
-        table+="<td title='border-c'>" + data.teams[i].lose +"</td>";
-        table+="<td title='border-c'>" + data.teams[i].goals +"</td>";
-        table+="<td title='border-c'>" + data.teams[i].conceded_goals +"</td>";
+        table+="<td title='border-r' id='matches'>" + data.teams[i].matches +"</td>";
+        table+="<td title='border-c' id='win'>" + data.teams[i].win +"</td>";
+        table+="<td title='border-c' id='draw'>" + data.teams[i].draw +"</td>";
+        table+="<td title='border-c' id='lose'>" + data.teams[i].lose +"</td>";
+        table+="<td title='border-c' id='goals'>" + data.teams[i].goals +"</td>";
+        table+="<td title='border-c' id='conc-goals'>" + data.teams[i].conceded_goals +"</td>";
         table+="<td class='score' title='border-l'>" + data.teams[i].score +"</td>";
         table+="</tr>"
         index++;
       }
-      table+="</tbody>";
+      table +="</tbody>"
       table+="</table>";
       table+="<p><span class='footnote'>М</span> - матчи, <span class='footnote'>В</span> - выигрыши, "+  // <--Примечание к турнирной таблице
              "<span class='footnote'>Н</span> - ничьи, <span class='footnote'>П</span> - проигрыши, "+
              "<span class='footnote'>Заб</span> - забитые голы, <span class='footnote'>Проп</span> - пропущенные голы, "+
              "<span class='footnote'>О</span> - очки в турнире</p>"
-      document.getElementById("App").innerHTML=table;                                              //Размещение сгенерированной таблицы в документе
+      document.getElementById("App").innerHTML=table;   //Размещение сгенерированной таблицы в документе
 });
